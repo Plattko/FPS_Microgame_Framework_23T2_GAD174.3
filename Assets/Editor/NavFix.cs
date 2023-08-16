becbe5d398565e414e0dd57259df34ab572a4841
Obi-Wan Codenobi <57944034+Joshua-J-G@users.noreply.github.com>
2023-08-17 01:37:49 +1000

    added My models
    also fixed the nav mesh
    and did some setup

---
+++ Assets/Editor/NavFix.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavFix : MonoBehaviour
+{
+    [MenuItem("Light Brigade/Debug/Force Cleanup NavMesh")]
+    public static void ForceCleanupNavMesh()
+    {
+        if (Application.isPlaying)
+            return;
+
+        NavMesh.RemoveAllNavMeshData();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
